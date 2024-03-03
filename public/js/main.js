8fe63729c48aaef484ac88253c1de7ad8838b241
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-03-03 22:07:55 +0100

    add firestore

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const db = firebase.firestore();
+    var user;
 });
 
+
+//handle login 
 document.getElementById('signInBtn').onclick = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth()
         .signInWithPopup(provider)
         .then((result) => {
-            var user = result.user;
+            var tempUser = result.user;
+
+            user = tempUser.multiFactor.user
             console.log(user)
 
             setLogged(true)
+
 
         }).catch((error) => {
             var errorCode = error.code;
         });
 }
-
-
-
+document.getElementById('signOutBtn').onclick = () => {
+    setLogged(false)
+}
 const setLogged = (bool) => {
     document.getElementById('whenSignedOut').hidden = bool;
     document.getElementById('whenSignedIn').hidden = !bool;
 }
+//----------------------------------------------------------------
